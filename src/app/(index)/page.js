9dbcc1a4c7c2058ae27d7e0a55327d619fbeb2ff
YoHaiYo <12si47bun@naver.com>
2024-10-14 21:54:58 +0900

    테일윈드, 폰트어썸 연결

---
+++ src/app/(index)/page.js
@@ -0,0 +1,10 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHome, faUser } from "@fortawesome/free-solid-svg-icons";
+export default function IndexPage() {
+  return (
+    <>
+      <FontAwesomeIcon icon={faUser} width={20} />
+      <h1>Hello, IndexPage!</h1>
+    </>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}", // src 폴더의 모든 파일에서 Tailwind CSS를 사용할 수 있도록 설정
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
