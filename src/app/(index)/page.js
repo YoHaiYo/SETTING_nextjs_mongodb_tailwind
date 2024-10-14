945de8a82875ec9dad7ffbb7081608e7d43d6030
YoHaiYo <12si47bun@naver.com>
2024-10-14 22:34:10 +0900

    navbar, 기본 section 스타일

--- src/app/(index)/page.js
+++ src/app/(index)/page.js
@@ -3,8 +3,50 @@ import { faHome, faUser } from "@fortawesome/free-solid-svg-icons";
 export default function IndexPage() {
   return (
     <>
-      <FontAwesomeIcon icon={faUser} width={20} />
-      <h1>Hello, IndexPage!</h1>
+      {/* <FontAwesomeIcon icon={faUser} width={20} />
+      <h1>Hello, IndexPage!</h1> */}
+
+      <section className="relative bg-white overflow-hidden">
+        <div className="py-20 md:py-28">
+          <div className="container px-4 mx-auto">
+            <div className="flex justify-center flex-wrap xl:items-center -mx-4">
+              <div className="w-full md:w-1/2 px-4 mb-16 md:mb-0">
+                <span className="inline-block py-px px-2 mb-4 text-xs leading-5 text-blue-500 bg-blue-100 font-medium uppercase rounded-full shadow-sm">
+                  Welcome
+                </span>
+                <h1 className="mb-6 text-3xl md:text-5xl lg:text-6xl leading-tight font-bold tracking-tight">
+                  Simplify Your URL Management Effortlessly.
+                </h1>
+                <p className="mb-8 text-lg md:text-xl text-coolGray-500 font-medium">
+                  Our platform makes saving and organizing URLs a breeze! Just
+                  paste a URL and hit enter to instantly save it, complete with
+                  an auto-generated favicon. Organize your links into categories
+                  and choose from various views—simple, grid, or list—to
+                  maximize your convenience.
+                </p>
+                <div className="flex flex-wrap justify-center">
+                  <div className="w-full md:w-auto py-1 md:py-0 md:mr-4">
+                    <a
+                      href="/login"
+                      className="inline-block py-3 px-5 w-full text-base md:text-lg leading-4 text-blue-50 font-medium text-center bg-blue-500 hover:bg-blue-600 focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 border border-blue-500 rounded-md shadow-sm"
+                    >
+                      Try It Now
+                    </a>
+                  </div>
+                  <div className="w-full md:w-auto py-1 md:py-0">
+                    <a
+                      href="/signup"
+                      className="inline-block py-3 px-5 w-full text-base md:text-lg leading-4 text-coolGray-800 font-medium text-center bg-white hover:bg-coolGray-100 focus:ring-2 focus:ring-coolGray-200 focus:ring-opacity-50 border border-coolGray-200 rounded-md shadow-sm"
+                    >
+                      Sign Up for Free
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
--- src/component/navbar/Navbar.js
+++ src/component/navbar/Navbar.js
@@ -1,10 +1,32 @@
 // src/components/Navbar.tsx
+"use client";
+
 import Link from "next/link";
+import { useState } from "react";
+import "./Navbar.css"; // 일반 CSS 파일 임포트
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const closeMenu = () => {
+    setIsOpen(false);
+  };
+
+  // 메뉴 링크 정의
+  const menuLinks = [
+    { href: "/", label: "Home" },
+    { href: "#", label: "Our services" },
+    { href: "#", label: "About" },
+    { href: "#", label: "Contact" },
+  ];
+
   return (
     <div className="top-0 py-1 lg:py-2 w-full bg-transparent lg:relative z-50 dark:bg-gray-900">
-      <nav className="z-10 sticky top-0 left-0 right-0 max-w-4xl xl:max-w-5xl mx-auto px-5 py-2.5 lg:border-none lg:py-4">
+      <nav className="z-10 sticky top-0 left-0 right-0 max-w-4xl xl:max-w-5xl mx-auto px-5xxx py-2.5 lg:border-none lg:py-4">
         <div className="flex items-center justify-between">
           <button>
             <div className="flex items-center space-x-2">
@@ -15,18 +37,14 @@ const Navbar = () => {
           </button>
           <div className="hidden lg:block">
             <ul className="flex space-x-10 text-base font-bold text-black/60 dark:text-white">
-              <li className="hover:underline hover:underline-offset-4 hover:w-fit transition-all duration-100 ease-linear">
-                <a href="#">Home</a>
-              </li>
-              <li className="hover:underline hover:underline-offset-4 hover:w-fit transition-all duration-100 ease-linear">
-                <a href="#">Our services</a>
-              </li>
-              <li className="hover:underline hover:underline-offset-4 hover:w-fit transition-all duration-100 ease-linear">
-                <a href="#">About</a>
-              </li>
-              <li className="hover:underline hover:underline-offset-4 hover:w-fit transition-all duration-100 ease-linear">
-                <a href="#">Contact</a>
-              </li>
+              {menuLinks.map((link) => (
+                <li
+                  key={link.label}
+                  className=" hover:text-blue-500 hover:w-fit transition-all duration-100 ease-linear"
+                >
+                  <Link href={link.href}>{link.label}</Link>
+                </li>
+              ))}
             </ul>
           </div>
           <div className="hidden lg:flex lg:items-center gap-x-2">
@@ -38,14 +56,17 @@ const Navbar = () => {
             </button>
           </div>
           <div className="flex items-center justify-center lg:hidden">
-            <button className="focus:outline-none text-slate-200 dark:text-white">
+            <button
+              onClick={toggleMenu}
+              className="focus:outline-none text-slate-200 dark:text-white"
+            >
               <svg
                 stroke="currentColor"
                 fill="currentColor"
                 strokeWidth={0}
                 viewBox="0 0 20 20"
                 aria-hidden="true"
-                className="text-2xl text-slate-800 dark:text-white focus:outline-none active:scale-110 active:text-violet-500"
+                className="text-2xl text-slate-800 dark:text-white focus:outline-none active:scale-110 active:text-blue-500"
                 height="1em"
                 width="1em"
                 xmlns="http://www.w3.org/2000/svg"
@@ -59,6 +80,67 @@ const Navbar = () => {
             </button>
           </div>
         </div>
+
+        {/* 모바일 메뉴 */}
+        <div
+          className={`fixed top-0 left-0 h-full w-64 bg-white dark:bg-gray-800 shadow-lg transform ${
+            isOpen ? "slide-in" : "slide-out"
+          } transition-transform duration-300 ease-in-out z-50`}
+        >
+          <div className="p-4">
+            <button onClick={closeMenu} className="absolute top-2 right-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6 text-black dark:text-white"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
+            <ul className="flex flex-col space-y-4 text-base font-bold text-black/60 dark:text-white">
+              {menuLinks.map((link) => (
+                <li
+                  key={link.label}
+                  className=" hover:text-blue-500 hover:w-fit transition-all duration-100 ease-linear"
+                >
+                  <Link href={link.href} onClick={closeMenu}>
+                    {link.label}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+            {/* 로그인 및 사인업 버튼 추가 */}
+            <div className="flex flex-col space-y-4 mt-4 absolute bottom-4 left-4 right-4">
+              <Link
+                href="#"
+                className="flex items-center justify-center px-6 py-2.5 font-semibold border border-gray-300 text-black dark:text-white"
+              >
+                Sign up
+              </Link>
+              <Link
+                href="#"
+                className="flex items-center justify-center rounded-md bg-[#4A3BFF] text-white px-6 py-2.5 font-semibold hover:shadow-lg hover:drop-shadow transition duration-200"
+              >
+                Login
+              </Link>
+            </div>
+          </div>
+        </div>
+
+        {/* 딤 배경 */}
+        {isOpen && (
+          <div
+            className={`menu-overlay show`}
+            onClick={closeMenu} // 딤 클릭 시 메뉴 닫기
+          />
+        )}
       </nav>
     </div>
   );
